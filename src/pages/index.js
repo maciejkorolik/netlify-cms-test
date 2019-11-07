d2a24cc879dd089a2d6c87dad92d9b13dd15d313
maciejkorolik <maciej@impresspro.pl>
2019-11-07 13:51:47 +0100

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,10 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
-
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
+import "../style.css"
 
 class BlogIndex extends React.Component {
   render() {
@@ -34,6 +34,7 @@ class BlogIndex extends React.Component {
               </header>
               <section>
                 <p
+                  className="paragraph"
                   dangerouslySetInnerHTML={{
                     __html: node.frontmatter.description || node.excerpt,
                   }}
